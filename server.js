b81dc74d5713eee28e34d8063f76ae5d629b3612
MikeB <burton.mj@gmail.com>
2017-03-07 16:50:12 -0700

    first commit on inclass branch

--- server.js
+++
@@ -1,9 +0,0 @@
-var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I can tell you how to do that."];
-
-var express = require('express');
-
-var app = express();
-
-app.listen(8887, function(){
-  console.log('Listening on port 8887');
-});
