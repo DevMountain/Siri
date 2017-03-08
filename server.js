2abadd9fce3b1e45014c8da380b60ca5b80cde88
MikeB <burton.mj@gmail.com>
2017-03-07 19:24:55 -0700

    finished with random messages working

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+
+var app = express();
+
+var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I can tell you how to do that."];
+
+
+app.listen(8887, function(){
+  console.log('listening on port 8887');
+})
+
+var getRandomMessage = function(){
+
+ var rand = messages[Math.floor(Math.random() * messages.length)];
+
+  return rand;
+
+
+}
+
+app.get('/', function(req, res){
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
+    message: getRandomMessage()
+  }))
+})
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+});
